ed0928077f186fe180461dbbaeea814e38a73841
Antonio Hamilton Gomes de Melo <hamdev@DESKTOP-CAQ2BTH>
2022-08-09 22:17:50 -0300

    update js file for question 6

--- questao06.mjs
+++ questao06.mjs
@@ -2,10 +2,12 @@ const estatistica = () => {
     let soma = 0;
     let media;
     let num = 0;
+    let contador = 0;
     while(num != -1){
         num = parseInt(prompt('Digite um número inteiro\nDigite -1 para encerrar'));
         soma += num;
+        contador += 1;
     }
-    media = soma/5;
+    media = soma/contador;
     console.log(`Soma = ${soma}\nMédia = ${media}`);
 }
