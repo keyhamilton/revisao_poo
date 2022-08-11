2f82be52cdde783d74c5b2d8a90624a99117d0a6
Antonio Hamilton Gomes de Melo <hamdev@DESKTOP-CAQ2BTH>
2022-08-11 18:53:16 -0300

    update question 1

--- questao01.mjs
+++ questao01.mjs
@@ -1,9 +1,12 @@
-// declaração de variáveis
-let num = 2.5;
-let intNum = parseInt(num);
-let anterior = intNum;
-let sucessor = intNum + 1;
+const proximo = () => {
 
-// resultado
-
-console.log(`O anterior de ${num} é ${anterior}, e o sucessor é ${sucessor}`);
+    // declaração de variáveis
+    let num = Number(prompt('Digite um número com casa decimal -> '));
+    let intNum = parseInt(num);
+    let anterior = intNum;
+    let sucessor = intNum + 1;
+    
+    // resultado
+    
+    console.log(`O anterior de ${num} é ${anterior}, e o sucessor é ${sucessor}`);
+}
