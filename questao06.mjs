618412a9b300f7b521f4e30c4e153ea2a8344c0d
Antonio Hamilton Gomes de Melo <hamdev@DESKTOP-CAQ2BTH>
2022-08-11 23:01:01 -0300

    update question 6

--- questao06.mjs
+++ questao06.mjs
@@ -3,14 +3,26 @@ const estatistica = () => {
     let media;
     let num = 0;
     let contador = 0;
+    let valores = [];
+    let desvio_padrao = 0;
     while(num != -1){
         num = parseInt(prompt('Digite um número inteiro\nDigite -1 para encerrar'));
-        if(num == -1){
+        if(num === -1){
             break;
         }
         soma += num;
         contador += 1;
+        valores.push(num);
     }
     media = soma/contador;
-    console.log(`Soma = ${soma}\nMédia = ${media}`);
+    desvio_padrao = desvio(valores, media);
+    console.log(`Soma = ${soma}\nMédia = ${media}\nDesvio padrão = ${desvio_padrao}`);
+}
+
+const desvio = (list, media) => {
+    let diff = 0;
+    for(let i = 0; i < list.length; i++){
+        diff += (list[i] - media)**2;
+    }
+    return Math.sqrt(diff/list.length);
 }
